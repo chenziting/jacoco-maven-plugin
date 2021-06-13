f150ebbd6553c4abe85ccadc55aee75d110d79dd
chenziting <ztchenscut@163.com>
2021-06-13 17:07:09 +0800

    增加内部类、匿名内部类文件覆盖率统计

--- src/org/jacoco/maven/DateAuthorAggregateReportMojo.java
+++ src/org/jacoco/maven/DateAuthorAggregateReportMojo.java
@@ -19,6 +19,7 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.comments.CommentsCollection;
 import com.github.javaparser.ast.comments.JavadocComment;
 import com.github.javaparser.javadoc.JavadocBlockTag;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.maven.artifact.Artifact;
@@ -35,6 +36,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.stream.Collectors.*;
 
@@ -103,12 +105,33 @@ public class DateAuthorAggregateReportMojo extends ReportAggregateMojo {
 	}
 
 	/**
-	 * Class文件信息
+	 * 原始class文件信息
+	 */
+	private static class RawClassFile {
+		private final String yearMonth; // 创建日期（yyyy年MM月）
+		private final String author; // 作者
+		private final File directory; // 文件目录
+		private final String pathname; // 文件路径，不带后缀名
+
+		public RawClassFile(String yearMonth, String author, File directory, String pathname) {
+			this.yearMonth = yearMonth;
+			this.author = author;
+			this.directory = directory;
+			this.pathname = pathname;
+		}
+
+		public File getDirectory() {
+			return directory;
+		}
+	}
+
+	/**
+	 * class文件信息
 	 */
 	private static class ClassFile {
-		private final String yearMonth;
-		private final String author;
-		private final File file;
+		private final String yearMonth; // 创建日期（yyyy年MM月）
+		private final String author; // 作者
+		private final File file; // class文件对象
 
 		public ClassFile(String yearMonth, String author, File file) {
 			this.yearMonth = yearMonth;
@@ -182,46 +205,77 @@ public class DateAuthorAggregateReportMojo extends ReportAggregateMojo {
 	}
 
 	/**
-	 * 按照日期和作者聚合Class文件
+	 * 按照文件夹聚合class文件
 	 */
-	private Map<String, Map<String, List<File>>> aggregateProjectClassFiles(MavenProject project) throws IOException {
+	private Map<File, List<RawClassFile>> groupClassFileByDirectory(MavenProject project) throws IOException {
 		FileFilter filter = new FileFilter(includes, excludes);
 		String sourceDirectory = project.getBuild().getSourceDirectory();
 		String outputDirectory = project.getBuild().getOutputDirectory();
 		String baselineDate = reformatDate(this.baselineDate);
-		Map<String, Map<String, List<File>>> aggregatedClassFiles = filter.getFiles(new File(sourceDirectory))
-				.parallelStream()
-				.filter(file -> file.getPath().endsWith(JAVA_FILE_SUFFIX))
-				.map(javaFile -> {
-					CommentTags commentTags = getJavaFileCommentTags(javaFile);
-					if (StringUtils.isEmpty(commentTags.date) || StringUtils.isEmpty(commentTags.author)) {
-						return null;
-					}
-					// 如果设置了基准日期，并且文件日期小于基准日期
-					if (!StringUtils.isEmpty(baselineDate) && commentTags.date.compareTo(baselineDate) < 0) {
-						return null;
-					}
-					// 转换为yyyy年MM月格式
-					String yearMonth = commentTags.date.substring(0, 4) + "年" + commentTags.date.substring(4, 6) + "月";
-					// 替换文件路径前缀
-					String pathname = outputDirectory + StringUtils.removeStart(javaFile.getPath(), sourceDirectory);
-					// 将后缀名.java替换成.class
-					String classFilePathname = StringUtils.removeEnd(pathname, JAVA_FILE_SUFFIX) + CLASS_FILE_SUFFIX;
-					getLog().info("Found class file: " + classFilePathname);
-					return new ClassFile(yearMonth, commentTags.author, new File(classFilePathname));
-				})
-				.filter(Objects::nonNull)
-				.collect(
+		return filter.getFiles(new File(sourceDirectory))
+			.parallelStream()
+			.filter(file -> file.getPath().endsWith(JAVA_FILE_SUFFIX))
+			.map(javaFile -> {
+				CommentTags commentTags = getJavaFileCommentTags(javaFile);
+				// 忽略作日期或者作者信息为空的文件
+				if (StringUtils.isEmpty(commentTags.date) || StringUtils.isEmpty(commentTags.author)) {
+					return null;
+				}
+				// 如果设置了基准日期，忽略文件日期小于基准日期
+				if (!StringUtils.isEmpty(baselineDate) && commentTags.date.compareTo(baselineDate) < 0) {
+					return null;
+				}
+				// 转换为yyyy年MM月格式
+				String yearMonth = commentTags.date.substring(0, 4) + "年" + commentTags.date.substring(4, 6) + "月";
+				// 替换文件路径前缀
+				String pathname = outputDirectory + StringUtils.removeStart(javaFile.getPath(), sourceDirectory);
+				// 移除后缀名.java
+				String classFilePathname = StringUtils.removeEnd(pathname, JAVA_FILE_SUFFIX);
+				// 父目录
+				File directory = new File(classFilePathname + CLASS_FILE_SUFFIX).getParentFile();
+				return new RawClassFile(yearMonth, commentTags.author, directory, classFilePathname);
+			})
+			.filter(Objects::nonNull)
+			.collect(groupingByConcurrent(RawClassFile::getDirectory));
+	}
+
+	/**
+	 * 按照日期和作者聚合class文件
+	 */
+	private Map<String, Map<String, List<File>>> aggregateProjectClassFiles(MavenProject project) throws IOException {
+		Map<File, List<RawClassFile>> groupedClassFiles = groupClassFileByDirectory(project);
+		Map<String, Map<String, List<File>>> aggregatedClassFiles = groupedClassFiles.entrySet()
+			.parallelStream()
+			.flatMap(entry -> {
+				File directory = entry.getKey();
+				File[] filesInDirectory = ObjectUtils.defaultIfNull(directory.listFiles(), new File[0]);
+				return entry.getValue().stream().flatMap(rawClassFile -> {
+					String classFilePathname = rawClassFile.pathname + CLASS_FILE_SUFFIX; // class文件本身
+					String innerClassFilePathnamePrefix = rawClassFile.pathname + "$"; // 内部类、匿名内部类文件
+					return Arrays.stream(filesInDirectory)
+						.filter(file -> {
+							String path = file.getPath();
+							// 查找原始class文件和其中定义的内部类、匿名内部类文件
+							return path.equals(classFilePathname) || path.startsWith(innerClassFilePathnamePrefix);
+						})
+						.map(file -> {
+							getLog().info("Found class file: " + file.getPath());
+							return new ClassFile(rawClassFile.yearMonth, rawClassFile.author, file);
+						});
+				});
+			})
+			.collect(
+				groupingByConcurrent( // 提升性能
+					ClassFile::getYearMonth,
 					groupingBy(
-						ClassFile::getYearMonth,
-						groupingBy(
-							ClassFile::getAuthor,
-							mapping(ClassFile::getFile, toList())
-						)
+						ClassFile::getAuthor,
+						ConcurrentHashMap::new, // 提升性能
+						mapping(ClassFile::getFile, toList())
 					)
-				);
-		Set<String> authors = aggregatedClassFiles.values().stream().map(Map::keySet).flatMap(Set::stream)
-				.collect(toSet());
+				)
+			);
+		Set<String> authors = aggregatedClassFiles.values().stream().map(Map::keySet)
+				.flatMap(Set::stream).collect(toSet());
 		getLog().info("Java source file authors: " + authors);
 		return aggregatedClassFiles;
 	}
